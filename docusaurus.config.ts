a2766e74dc1185c384954368ed1e4c4a09ef6a5b
ledboot <popmusicbbq@gmail.com>
2025-04-11 20:02:09 +0800

    feat: 🎸 enhance Solidity documentation and add support for additional languages in syntax highlighting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -65,15 +65,15 @@ const config: Config = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'robots', content: 'index, follow' },
     ],
-    algolia: {
-      appId: '1CCGLI5COR',
-      apiKey: 'f47fda380029faa9451ba4dd591207e3',
-      indexName: 'wawov',
-      contextualSearch: true,
-      searchParameters: {},
-      searchPagePath: 'search',
-      insights: true,
-    },
+    // algolia: {
+    //   appId: '1CCGLI5COR',
+    //   apiKey: 'f47fda380029faa9451ba4dd591207e3',
+    //   indexName: 'wawov',
+    //   contextualSearch: true,
+    //   searchParameters: {},
+    //   searchPagePath: 'search',
+    //   insights: true,
+    // },
     navbar: {
       title: 'wawov',
       logo: {
@@ -132,6 +132,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['solidity', 'bash'],
     },
   } satisfies Preset.ThemeConfig,
 
